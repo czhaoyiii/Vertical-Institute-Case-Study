620f89cc5670129dbe1da2beefc83cf92c463eea
Chan Zhao Yi <123791392+czhaoyiii@users.noreply.github.com>
2026-05-08 00:25:26 +0800

    VI case study

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,6 +2,11 @@ class StudentsController < ApplicationController
   def show
     @student = Student.find(params[:id])
     enrollments = @student.enrollments.includes(:course, :activities, :ai_summaries).by_risk
-    @primary_enrollment = enrollments.first
+    @primary_enrollment =
+      if params[:enrollment_id].present?
+        enrollments.find { |e| e.id.to_s == params[:enrollment_id].to_s } || enrollments.first
+      else
+        enrollments.first
+      end
   end
 end
